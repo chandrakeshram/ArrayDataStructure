1bcb596de40d96f99a1183b6f88b789ca1696932
Chandrakesh Ram <79987328+chandrakeshram@users.noreply.github.com>
2022-01-02 20:23:31 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -4,25 +4,7 @@ Write a program to implement binary search in cpp.
 */
 #include <bits/stdc++.h>
 using namespace std;
-int binarySearch(int arr[],int n,int key){
-    int s=0;
-    int e=n;
-    while(s<=e){
-        int mid=(s+e)/2;
-        if(arr[mid]==key){
-            return mid;
-        }
-        else if(arr[mid]<key){
-            s=mid+1;
-            
-        }
-        else {
-            e=mid-1;
-            
-        }
-    }
-    return -1;
-}
+
 int main(){
     int n;
     cout<<"Enter the size of the array :"<<endl;
@@ -37,7 +19,29 @@ int main(){
     }
     cout<<"Enter the value that you want to search :"<<endl;
     cin>>key;
-    cout<<binarySearch(arr,n,key)<<endl;
+    int s=0;
+    int e=n;
+    int mid=(s+e)/2;
+    while(s<=e){
+        
+        if(arr[mid]==key){
+            cout<<"Search is successful . Key is found"<<endl;
+            break;
+        }
+        else if(arr[mid]<key){
+            s=mid+1;
+            
+        }
+        else {
+            e=mid-1;
+            
+        }
+        mid=(s+e)/2;
+    }
+    if(s>e){
+        cout<<"Search is unsuccessful . Key is not found"<<endl;
+    }
+    
     
     
     
@@ -49,17 +53,17 @@ int main(){
 Enter the size of the array :
 5
 Enter the elements of the array :
-78 96 47 58 321
+78 2 36 47 22
 Enter the value that you want to search :
-444
-Key is not found . Search is unsuccessful
+44
+Search is unsuccessful . Key is not found
 ===============================================
 Enter the size of the array :
 5
 Enter the elements of the array :
-78 96 47 2 66
+78 96 54 85 63
 Enter the value that you want to search :
-47
-Key is found . Search is successful
-==========================================
+63
+Search is successful . Key is found
+===============================================
 */
