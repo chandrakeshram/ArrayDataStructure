f88e3cb6e12b30d2b3b8d2f8d28ff80ed6095a26
Chandrakesh Ram <79987328+chandrakeshram@users.noreply.github.com>
2022-01-05 08:30:23 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -37,3 +37,12 @@ Sorted Array will be
 3 4 55 78 96
 ============================================
 */
+
+/*In bubble sort we will sort adjacent elements until we get the largest element at the last position
+  So the last elements is now sorted so there is no need to run the loop for last elements. Again we 
+  will follow same procedure and will swap until we get the second largest element at the second last 
+  position.Similarly we will do for the entire array.
+  Here we have to maintain a counter variable for avoiding the loop for the sorted part of the array 
+  We will run the inner loop from 0 to n-counter in which counter will be initialized with 1 and will
+  increment in while loop unless and until it is less than array size.
+  We will get the sorted array after the entire procedure .*/
